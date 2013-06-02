11c28cbb8853226ccbd198aef1d4db800f1f9d0b
Michael Pratt <pratt@hablarmierda.net>
2013-06-01 22:40:46 -0500

    Created the ServiceAdapter class, based on the old one

---
+++ Lib/SimpleLifestream/ServiceAdapter.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * ServiceAdapter.php
+ * Every service should extend this class.
+ *
+ * @author  Michael Pratt <pratt@hablarmierda.net>
+ * @link    http://www.michael-pratt.com/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+namespace SimpleLifestream;
+
+abstract class ServiceAdapter
+{
+    protected $resource;
+
+    /**
+     * Gets the data API response and returns an array
+     * with all the information.
+     *
+     * @return array
+     */
+    abstract public function getApiData();
+
+    /**
+     * Constructor
+     *
+     * @param object $http Instance of \SimpleLifestream\Interfaces\IHttp
+     * @param string $resource
+     * @return void
+     */
+    public function __construct(\SimpleLifestream\IHttp $http, $resource)
+    {
+        $this->http = $http;
+        $this->resource = $resource;
+    }
+}
+
+?>
