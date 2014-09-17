d64bdfc9a36698a36140149efaf4742216d480a8
Victor Garcia <victor.garcia@punchkickinteractive.com>
2014-09-17 11:22:44 -0500

    finish unit tests for Pinboard service

--- Tests/TestServicePinboard.php
+++ Tests/TestServicePinboard.php
@@ -15,10 +15,33 @@ class TestServicePinboard extends TestService
         'bookmarked',
     );
 
-    /*
     public function testRealRequest()
     {
-        $stream = $this->getStream('Pinboard', '');
+        if (!is_file(__DIR__ . '/AuthCredentials.php'))
+        {
+            $this->markTestSkipped('No Pinboard Credentials Found');
+            return ;
+        }
+
+        if (!ini_get('allow_url_fopen'))
+        {
+            $this->markTestIncomplete(
+                'Could not test Pinboard with file_get_contents, allow_url_fopen is closed'
+            );
+            return ;
+        }
+
+        require __DIR__ . '/AuthCredentials.php';
+
+        if (empty($pinboardToken))
+        {
+            $this->markTestIncomplete(
+                'Pinboard credentials are not provided in AuthCredentials.php'
+            );
+            return ;
+        }
+
+        $stream = $this->getStream('Pinboard', $pinboardToken);
         $response = $stream->getResponse();
 
         $this->checkResponseIntegrity('Pinboard', $response);
@@ -26,42 +49,37 @@ class TestServicePinboard extends TestService
         $errors = $stream->getErrors();
         $this->assertTrue(empty($errors));
     }
-    */
 
     public function testRequestFail()
     {
-        $stream = $this->getStream('Pinboard', 'unknown-or-invalid-pinboard-user');
+        $stream = $this->getStream('Pinboard', 'invalid-pinboard-token');
         $stream->getResponse();
 
         $errors = $stream->getErrors();
         $this->assertTrue(!empty($errors));
     }
 
-    /*
     public function testService()
     {
-        $stream = $this->getStream('Pinboard', 'dummySample1', 'vicgarcia-2014-09-16');
+        $stream = $this->getStream('Pinboard', 'dummySample', 'vicg4rcia-2014-09-17');
         $response = $stream->getResponse();
 
         $this->checkResponseIntegrity('Pinboard', $response);
 
-        $this->assertEquals(10, count($response));
+        $this->assertEquals(15, count($response));
 
         $errors = $stream->getErrors();
         $this->assertTrue(empty($errors));
     }
-    */
 
-    /*
     public function testServiceInvalidAnswer()
     {
-        $stream = $this->getStream('Pinboard', 'dummyInvalidResourceNotJson', 'this is not a response');
+        $stream = $this->getStream('Pinboard', 'dummyInvalidResponse', 'an invalid response');
         $stream->getResponse();
 
         $errors = $stream->getErrors();
         $this->assertTrue(!empty($errors));
     }
-    */
 }
 ?>
 
